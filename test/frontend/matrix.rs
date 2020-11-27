b997a8b036766a84adfa927e380893b876d971b1
anurudhp <anurudhp97@gmail.com>
2020-11-27 17:19:37 +0530

    [frontend] fix unresolved test

--- test/frontend/matrix.rs
+++ test/frontend/matrix.rs
@@ -1,3 +1,5 @@
+// RUN: ../../build/bin/frontend %s -interpret
+
 struct MatrixSequence(i64, i64);
 fn reduce1d(m: !MatrixSequence) -> !i64 {
     return 42;
@@ -7,4 +9,4 @@ fn main() -> !i64 {
     let m : !MatrixSequence  = MatrixSequence(0, 4040);
     let s : !i64 = reduce1d!(m);
     return s;
-}                                                                                                                     
+}
