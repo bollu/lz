5b42003998268b2271808e8c3caf13a3c607c857
Siddharth Bhat <siddu.druid@gmail.com>
2021-01-06 14:49:28 +0530

    OK, we do get squeaky clean LLVM

--- run-optimized.sh
+++ run-optimized.sh
@@ -6,7 +6,7 @@ clang++ -DNODEBUG -O2 -S -emit-llvm -I /home/bollu/work/mlir/lz/include/ /home/b
 # sed -i "s/attributes #6 = {/attributes #6 = {norecurse alwaysinline argmemonly /g" runtime.ll
 # sed -i "s/attributes #7 = {/attributes #7 = {norecurse alwaysinline argmemonly /g" runtime.ll
 
-hask-opt $@ --lz-worker-wrapper --lz-lower --convert-scf-to-std --ptr-lower -o /tmp/out.mlir
+hask-opt $@ --lz-worker-wrapper --affine-loop-fusion --canonicalize --lz-lower --convert-scf-to-std --ptr-lower -o /tmp/out.mlir
 mlir-translate /tmp/out.mlir --mlir-to-llvmir -o /tmp/out.ll
 llvm-link /home/bollu/work/mlir/lz/runtime.ll /tmp/out.ll -S -o /tmp/out-linked.ll
 opt -always-inline -O3 -S /tmp/out-linked.ll -o /tmp/out-linked-o3.ll
