522954498cb96c196ef2ac5595631ad4831df0a8
Siddharth Bhat <siddu.druid@gmail.com>
2021-02-17 01:53:05 +0530

    pulling in the AST -> MLIR translator. All hell breaks loose~

--- lambdapure-translate/lambdapure-translate.cpp
+++ lambdapure-translate/lambdapure-translate.cpp
@@ -5,9 +5,16 @@
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Translation.h"
 
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/ScopedHashTable.h"
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Casting.h"
+#include "llvm/Support/raw_ostream.h"
+
+
 // https://github.com/llvm/llvm-project/blob/e21adfa32d8822f9ea4058c3e365a841d87cb3ee/mlir/lib/Target/LLVMIR/ConvertFromLLVMIR.cpp
 using namespace mlir;
-
+using namespace llvm;
 
 // === AST ===
 // === AST ===
@@ -596,7 +603,7 @@ private:
   // std::string curLineBuffer = "\n";
 
 public:
-  Lexer(std::string filename, llvm::StringRef buffer)
+  Lexer(llvm::StringRef buffer)
       : context(nullptr), 
         lastLocation(mlir::FileLineColLoc::get("UNKNOWN-FILE", 1, 1, context)),
         buffer(buffer) {}
@@ -861,38 +868,280 @@ public:
 }; // class Parser
 
 
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-// === IMPORTER ===
-//
-struct Importer {
-  Importer(MLIRContext *context, ModuleOp module)
-      : b(context), context(context), module(module),
-        unknownLoc(FileLineColLoc::get("imported-bitcode", 0, 0, context)) {
-    b.setInsertionPointToStart(module.getBody());
-    context->isMultithreadingEnabled();
-    (void)context;
-    (void)currentEntryBlock;
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+// === MLIRGEN ===
+class MLIRGenImpl {
+public:
+  MLIRGenImpl(mlir::MLIRContext &context)
+      : builder(&context), lastLoc(builder.getUnknownLoc()) {}
+  // converts lambdapure AST -> MLIR
+  mlir::ModuleOp mlirGen(ModuleAST &moduleAST) {
+    theModule = mlir::ModuleOp::create(loc());
+    auto funcs = moduleAST.getFList();
+    for (auto &funcAST : funcs) {
+      mlir::FuncOp func = mlirGen(*funcAST);
+      if (!func) {
+        return nullptr;
+      }
+      theModule.push_back(func);
+    }
+    // for(int i = 0; i < funcs.size(); ++i){
+    //   // auto func = mlirGen(*funcs.at(i));
+    // }
+    // for(FunctionAST &funcAST : moduleAST.getFList()){
+    //   // auto func = mlirGen(funcAST);
+    //   // if(!func){
+    //   //   return nullptr;
+    //   // }
+    //   // theModule.push_back(func);
+    // }
+
+    // if (failed(mlir::verify(theModule))) {
+    //   theModule.emitError("module verification error");
+    //   return nullptr;
+    // }
+    return theModule;
+  }
+
+  mlir::ModuleOp emptyModuleOp() {
+    return mlir::ModuleOp::create(builder.getUnknownLoc());
   }
 
 private:
-  OpBuilder b;
-  /// The current context.
-  __attribute__((unused)) MLIRContext *context;
-  /// The current module being created.
-  ModuleOp module;
-  /// The entry block of the current function being processed.
-  Block *currentEntryBlock;
-  /// Unknown location
-  Location unknownLoc;
+  mlir::ModuleOp theModule;
+  mlir::OpBuilder builder;
+  llvm::ScopedHashTable<StringRef, mlir::Value> symbolTable;
+  ScopeTable scopeTable = ScopeTable();
+  mlir::Location lastLoc;
+  mlir::Block lastBlock;
+  mlir::Location loc() { return lastLoc; }
+
+  mlir::Location loc(Location loc) {
+    lastLoc = loc;
+    return loc;
+  }
+
+  mlir::Type typeGen(VarType t) {
+    switch (t) {
+    case object:
+      return ObjectType::get(builder.getContext());
+    case u8:
+      return builder.getIntegerType(8);
+    default:
+      return builder.getIntegerType(64);
+    }
+  }
+
+  mlir::FuncOp mlirGen(FunctionAST &functionAST) {
+    // create scope
+    scopeTable.scope();
+    ScopedHashTableScope<StringRef, mlir::Value> var_scope(symbolTable);
+    // setup type
+    std::vector<mlir::Type> inputs;
+    for (VarType t : functionAST.getArgTypes()) {
+      inputs.push_back(typeGen(t));
+    }
+    mlir::Type retTy;
+    auto FName = functionAST.getName();
+    auto func_type =
+        builder.getFunctionType(inputs, typeGen(functionAST.getRetType()));
+    // if(FName.compare("main") == 0){
+    //   FName = "main";
+    //   // if(inputs.size() > 0 ) {assert(false && "Invalid function main_, no
+    //   input types allowed");} func_type =
+    //   builder.getFunctionType(inputs,llvm::None);
+    // }
+
+    // setup loc,name,type
+    //--------------------------------------------------------
+    mlir::FuncOp function =
+        mlir::FuncOp::create(loc(functionAST.getLoc()), FName, func_type);
+    mlir::Block &entryBlock = *function.addEntryBlock();
+
+    for (unsigned i = 0; i < functionAST.getArgs().size(); ++i) {
+      if (failed(scopeTable.declare(
+              functionAST.getArgs().begin()[i]->getName(),
+              entryBlock.getArguments().begin()[i],
+              typeGen(functionAST.getArgTypes().begin()[i])))) {
+        return nullptr;
+      }
+    }
+    //
+    builder.setInsertionPointToStart(&entryBlock);
+    // auto region = function.getCallableRegion();
+    mlirGen(*functionAST.getFBody());
+
+    //--------------------------------------------------------
+    // experimenting
+    // ReturnOp returnOp;
+    // builder.create<ReturnOp>(loc());
+    //===============
+
+    scopeTable.descope();
+    return function;
+  }
+
+  mlir::LogicalResult mlirGen(DirectRetStmtAST &direct) {
+    llvm::StringRef var = direct.getVar();
+    mlir::Value result = scopeTable.lookup(var);
+    builder.create<ReturnOp>(loc(), result);
+    // if(!result){
+    //   builder.create<ReturnOp>(loc());
+    //
+    // }
+    // else {
+    //   builder.create<ReturnOp>(loc(),result);
+    // }
+    return mlir::success();
+  }
+
+  mlir::LogicalResult mlirGen(CaseStmtAST &casestmt) {
+    llvm::StringRef var = casestmt.getVar();
+    mlir::Value curr_val = scopeTable.lookup(var);
+    auto bodies = casestmt.getBodies();
+    mlir::Type t = curr_val.getType();
+    if (t.isa<ObjectType>()) {
+      curr_val =
+          builder.create<TagGetOp>(loc(), builder.getIntegerType(8), curr_val);
+    }
+    auto caseOp = builder.create<CaseOp>(loc(), curr_val, bodies.size());
+    int i = 0;
+    for (auto &body : bodies) {
+      auto &region = caseOp.getRegion(i);
+      auto block = builder.createBlock(&region);
+      builder.setInsertionPointToStart(block);
+      mlirGen(*body);
+      ++i;
+    }
+    return mlir::success();
+  }
+
+  mlir::LogicalResult mlirGen(RetStmtAST &ret) {
+    switch (ret.getKind()) {
+    case RetStmtAST::Direct:
+      return mlirGen(cast<DirectRetStmtAST>(ret));
+    case RetStmtAST::Case:
+      return mlirGen(cast<CaseStmtAST>(ret));
+    default:
+      return mlir::failure();
+    }
+  }
+
+  mlir::Value mlirGen(NumberExprAST &expr) {
+    return builder.create<IntegerConstOp>(loc(), expr.getValue());
+  }
+
+  mlir::Value mlirGen(VariableExprAST &expr) {
+    return scopeTable.lookup(expr.getName());
+  }
+
+  mlir::Value mlirGen(AppExprAST &expr, mlir::Type ty) {
+    std::vector<mlir::Value> args = std::vector<mlir::Value>();
+
+    mlir::Value funcVal = scopeTable.lookup(expr.getFName());
+    for (auto &varExpr : expr.getArgs()) {
+      args.push_back(mlirGen(*varExpr));
+    }
+    return builder.create<AppOp>(loc(), funcVal, args, ty);
+  }
+
+  mlir::Value mlirGen(CallExprAST &expr, mlir::Type ty) {
+    std::vector<mlir::Value> args = std::vector<mlir::Value>();
+    for (auto &varExpr : expr.getArgs()) {
+      args.push_back(mlirGen(*varExpr));
+    }
+    std::string fName = expr.getFName();
+    std::vector<mlir::Type> results;
+    results.push_back(ty);
+    return builder.create<CallOp>(loc(), fName, args, ty);
+  }
+
+  mlir::Value mlirGen(PapExprAST &expr, mlir::Type ty) {
+    std::vector<mlir::Value> args = std::vector<mlir::Value>();
+    for (auto &varExpr : expr.getArgs()) {
+      args.push_back(mlirGen(*varExpr));
+    }
+    std::string fName = expr.getFName();
+    return builder.create<PapOp>(loc(), fName, args);
+  }
+
+  mlir::Value mlirGen(ProjExprAST &expr, mlir::Type ty) {
+    auto varExpr = expr.getVar();
+    return builder.create<ProjectionOp>(loc(), expr.getIndex(),
+                                        mlirGen(*varExpr), ty);
+  }
+
+  mlir::Value mlirGen(CtorExprAST &expr, mlir::Type ty) {
+    std::vector<mlir::Value> args = std::vector<mlir::Value>();
+    for (auto &varExpr : expr.getArgs()) {
+      args.push_back(mlirGen(*varExpr));
+    }
+    return builder.create<ConstructorOp>(loc(), expr.getTag(), args, ty);
+    // return nullptr;
+  }
+
+  mlir::Value mlirGen(ExprAST &expr, mlir::Type ty) {
+
+    switch (expr.getKind()) {
+    case ExprAST::NumberExpr:
+      return mlirGen(cast<NumberExprAST>(expr));
+    case ExprAST::VarExpr:
+      return mlirGen(cast<VariableExprAST>(expr));
+    case ExprAST::AppExpr:
+      return mlirGen(cast<AppExprAST>(expr), ty);
+    case ExprAST::CallExpr:
+      return mlirGen(cast<CallExprAST>(expr), ty);
+    case ExprAST::CtorExpr:
+      return mlirGen(cast<CtorExprAST>(expr), ty);
+    case ExprAST::ProjExpr:
+      return mlirGen(cast<ProjExprAST>(expr), ty);
+    case ExprAST::PapExpr:
+      return mlirGen(cast<PapExprAST>(expr), ty);
+    default:
+      std::cout << "Invalid expression found in lambdapure AST" << std::endl;
+      return nullptr;
+    }
+  }
+
+  mlir::LogicalResult mlirGen(FBodyAST &fBodyAST) {
+    for (const std::unique_ptr<LetStmtAST> &letstmt : fBodyAST.getStmts()) {
+      auto expr = letstmt->getExpr();
+      mlir::Type ty = typeGen(letstmt->getVtype());
+      mlir::Value value = mlirGen(*expr, ty);
+      if (failed(scopeTable.declare(letstmt->getName(), value, ty))) {
+        std::cout << "Failed to declare variable in let stament" << std::endl;
+        return mlir::failure();
+      }
+    }
+    auto ret = mlirGen(*fBodyAST.getRet());
+    return ret;
+  }
 };
+
+
+mlir::OwningModuleRef mlirGen(mlir::MLIRContext &context,
+                              ModuleAST &moduleAST) {
+  return MLIRGenImpl(context).mlirGen(moduleAST);
+}
+
+
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
+//  === translateLambdapureToModule === 
 
 OwningModuleRef translateLambdapureToModule(llvm::SourceMgr &sourceMgr,
                                             MLIRContext *context) {
@@ -909,11 +1158,21 @@ OwningModuleRef translateLambdapureToModule(llvm::SourceMgr &sourceMgr,
   //   emitError(UnknownLoc::get(context)) << errStream.str();
   //   return {};
   // }
-  context->loadDialect<standalone::HaskDialect>();
-  OwningModuleRef module(ModuleOp::create(
-      FileLineColLoc::get("", /*line=*/0, /*column=*/0, context)));
 
-  Importer deserializer(context, module.get());
+  const llvm::MemoryBuffer *file =
+      sourceMgr.getMemoryBuffer(sourceMgr.getMainFileID());
+
+  llvm::StringRef buffer = file->getBuffer();
+  Lexer lexer = Lexer(buffer);
+  Parser parser = Parser(lexer);
+  std::unique_ptr<ModuleAST> lambdapureModule = parser.parse();
+
+
+  context->loadDialect<standalone::HaskDialect>();
+  // OwningModuleRef module(ModuleOp::create(
+  //     FileLineColLoc::get("", /*line=*/0, /*column=*/0, context)));
+
+  // Importer deserializer(context, module.get());
   // for (llvm::GlobalVariable &gv : llvmModule->globals()) {
   //   if (!deserializer.processGlobal(&gv))
   //     return {};
@@ -923,7 +1182,7 @@ OwningModuleRef translateLambdapureToModule(llvm::SourceMgr &sourceMgr,
   //     return {};
   // }
 
-  return module;
+  return mlirGen(*context, *lambdapureModule);
 }
 
 int main(int argc, char **argv) {
