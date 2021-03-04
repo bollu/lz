1521d4e9e6cc0db0349dbe414cd4fb4d8fe89929
Siddharth Bhat <siddu.druid@gmail.com>
2021-03-04 22:30:21 +0530

    get expression to pass
    Failed Tests (4):
      HASK_OPT :: lambdapure/bench/loop-defunc.lean
      HASK_OPT :: lambdapure/bench/loop.lean
      HASK_OPT :: lambdapure/bench/sort.lean
      HASK_OPT :: lambdapure/bench/treemap.lean
    
    Testing Time: 10.54s
      Passed    : 9
      Unresolved: 6
      Failed    : 4

--- test/lambdapure/bench/expression.lean
+++ test/lambdapure/bench/expression.lean
@@ -2,19 +2,20 @@
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates --lz-lambdapure-reference-rewriter | FileCheck %s
-
 -- CHECK: func @const_fold
 
 set_option trace.compiler.ir.init true
+
+
 inductive Expr
 | Val : Nat → Expr
-| Add : Expr → Expr → Expr
-| Mul : Expr → Expr → Expr
+| EAdd : Expr → Expr → Expr
+| EMul : Expr → Expr → Expr
 
 open Expr
 
 def const_fold : Expr -> Expr
 | Val n => Val n
-| Add (Val x) (Val y) => Val (x + y)
-| Mul (Val x) (Val y) => Val (x * y)
+| EAdd (Val x) (Val y) => Val (x + y)
+| EMul (Val x) (Val y) => Val (x + y)
 | e => e
