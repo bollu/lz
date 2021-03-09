43c0c76f6be31a6c43f4ea60ba535e71786f7317
Siddharth Bhat <siddu.druid@gmail.com>
2021-03-09 20:00:16 +0530

    minimise

--- test/lambdapure/simple/jmp.lean
+++ test/lambdapure/simple/jmp.lean
@@ -3,14 +3,13 @@
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates --lz-lambdapure-reference-rewriter | FileCheck %s
 
--- XXXXX: func @main
+-- : func @constFolding
 -- CHECK: module {
 
 set_option trace.compiler.ir.init true
 
 
 inductive Expr
-| Var : Nat → Expr
 | Val : Nat → Expr
 | Add : Expr → Expr → Expr
 | Mul : Expr → Expr → Expr
@@ -19,26 +18,11 @@ namespace Expr
 open Nat
 
 def constFolding : Expr → Expr
-| Add e₁ e₂   =>
-  let e₁ := constFolding e₁;
-  let e₂ := constFolding e₂;
-  (match e₁, e₂ with
-   | Val a, Val b         => Val (a+b)
+| Add e1 e2   =>
+  (match e1, e2 with
    | Val a, Add e (Val b) => Add (Val (a+b)) e
    | Val a, Add (Val b) e => Add (Val (a+b)) e
-   | _,     _             => Add e₁ e₂)
+   | _,     _             => Add e1 e2)
 | e         => e
 
 end Expr
--- 
--- open Expr
--- 
--- unsafe def main : List String → IO UInt32
--- | [s] => do
---   let n := s.toNat!;
---   let e  := (mkExpr n 1);
---   let v₁ := eval e;
---   let v₂ := eval (constFolding (reassoc e));
---   IO.println (toString v₁ ++ " " ++ toString v₂);
---   pure 0
--- | _ => pure 1
