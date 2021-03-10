91475d543ac514e5dee83697d9d79e070bca1ca6
Siddharth Bhat <siddu.druid@gmail.com>
2021-03-10 22:46:19 +0530

    add speedup plot and data

--- test/plotting/speedup_data.py
+++ test/plotting/speedup_data.py
@@ -10,7 +10,6 @@ import sys
 def log(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-
 parser = argparse.ArgumentParser(description='Run MLIR files to document number of thunks dropped')
 parser.add_argument('--out', metavar='o', type=str,
                     help='path to dump output')
@@ -18,30 +17,78 @@ parser.add_argument('--out', metavar='o', type=str,
 args = parser.parse_args()
 
 g_opt = sh.hask_opt
-g_fpaths = glob.glob("../**/*.mlir"); g_nfiles = len(g_fpaths)
+g_fpaths = glob.glob("../**/*.mlir")
+g_fpaths.extend(glob.glob("../*.mlir"))
+g_fpaths = []
+g_fpaths.append("../grin/sum-upto-grin.mlir")
+# g_fpaths.append("../k-lazy.mlir")
+# g_fpaths.append("../foo.mlir")
+g_fpaths.append("../rec.mlir")
+# g_fpaths.append("../worker-wrapper-fact.mlir")
+# g_fpaths.append("../case-of-boxed-recursive-ap-with-final-construct.mlir")
+# g_fpaths.append("../worker-wrapper-int-tail-recursive.mlir")
+# g_fpaths.append("../add.mlir")
+g_fpaths.append("../worker-wrapper-int-non-tail-recursive.mlir")
+g_fpaths.append("../outline-return-of-constructor.mlir")
+# g_fpaths.append("../either.mlir")
+g_fpaths.append("../fib.mlir")
+g_fpaths.append("../outline-case-of-fn-input.mlir")
+# g_fpaths.append("../Interpreter/stdops.mlir")
+# g_fpaths.append("../Lowering/lower-ap-and-force-2.mlir")
+# g_fpaths.append("../Lowering/lower-construct-return.mlir")
+# g_fpaths.append("../Lowering/lower-add.mlir")
+# g_fpaths.append("../Lowering/lower-case-int.mlir")
+# g_fpaths.append("../Lowering/lower-lz-and-memref.mlir")
+# g_fpaths.append("../Lowering/lower-ap-and-force.mlir")
+# g_fpaths.append("../Lowering/lower-case-single-alt-no-args.mlir")
+# g_fpaths.append("../Lowering/lower-construct-memref.mlir")
+# g_fpaths.append("../Lowering/lower-apeager.mlir")
+# g_fpaths.append("../Lowering/lower-thunkify.mlir")
+# g_fpaths.append("../Lowering/lower-lz-and-affine.mlir")
+g_fpaths.append("../fib-strict.mlir")
+# g_fpaths.append("../updatable-thunks-infer-non-upatable.mlir")
+g_fpaths.append("../worker-wrapper-maybe-non-tail-recursive.mlir")
+# g_fpaths.append("../worker-wrapper-maybe-tail-recursive.mlir")
+# g_fpaths.append("../case-usage.mlir")
+# g_fpaths.append("../case-int.mlir")
+# g_fpaths.append("../optimize-linalg.mlir")
+g_nfiles = len(g_fpaths)
 
 g_data = []
 for (i, fpath) in enumerate(g_fpaths):
-    log(f"[{i+1:3}/{g_nfiles}] |{fpath}|")
     data = {"file": str(fpath)}
     try:
         call = g_opt(fpath, "--lz-interpret", "-o", "/dev/null").wait()
         data["baseline"] = {}
-        data["baseline"]["stdout"] = call.stdout.decode()
-        data["baseline"]["stderr"] = call.stderr.decode()
+        stdout = call.stdout.decode()
+        data["baseline"]["nthunks"] = int(stdout.split("num_thunkify_calls(")[1].split(")")[0])
+        data["baseline"]["nconstructs"] = int(stdout.split("num_construct_calls(")[1].split(")")[0])
+        stdout = None
+
 
         call = g_opt(fpath, "--lz-worker-wrapper", "--lz-interpret", "-o", "/dev/null").wait()
         data["optimised"] = {}
-        data["optimised"]["stdout"] = call.stdout.decode()
-        data["optimised"]["stderr"] = call.stderr.decode()
+        # data["optimised"]["stdout"] = call.stdout.decode()
+        # data["optimised"]["stderr"] = call.stderr.decode()
+        stdout = call.stdout.decode()
+        data["optimised"]["nthunks"] = int(stdout.split("num_thunkify_calls(")[1].split(")")[0])
+        data["optimised"]["nconstructs"] = int(stdout.split("num_construct_calls(")[1].split(")")[0])
+
+        baseline = data["baseline"]["nthunks"] + data["baseline"]["nconstructs"]
+
+        if baseline <= 20:
+            raise RuntimeError(fpath)
 
         data["success"] = True
+        g_data.append(data)
+        log(f"success [{i+1:3}/{g_nfiles}] |{fpath}|")
     except Exception as e:
-        data["success"] = False
-        data["err"] = str(e)
-    g_data.append(data)
+        log(f"failure [{i+1:3}/{g_nfiles}] |{fpath}|")
+        raise e
+      # data["success"] = False
+      # data["err"] = str(e)
 
-print(json.dumps(g_data, indent=1))
+# print(json.dumps(g_data, indent=1))
 
 with open("speedup_data.json", "w") as f:
     json.dump(g_data, f, indent=2)
--- test/plotting/speedup_plot.py
+++ test/plotting/speedup_plot.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
 
-import glob
 import os
+import os.path
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-import sh
+import json
+import sys
+
+def log(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
 
 # Use TrueType fonts instead of Type 3 fonts
 # Type 3 fonts embed bitmaps and are not allowed in camera-ready submissions
@@ -15,9 +19,9 @@ matplotlib.rcParams['ps.fonttype'] = 42
 
 matplotlib.rcParams['figure.figsize'] = 5, 2
 
-labels = ['G1', 'G2', 'G3', 'G4', 'G5']
-men_means = [1.5, 1.2, 1.3, 1.1, 1.0]
-women_means = [1.8, 1.5, 1.1, 1.3, 0.9]
+labels = []
+baselines = []
+optims = []
 
 # Color palette
 light_gray = "#cacaca"
@@ -29,50 +33,68 @@ dark_green = "#33a02c"
 light_red = "#fb9a99"
 dark_red = "#e31a1c"
 
-def foo():
-    x = np.arange(len(labels))  # the label locations
-    width = 0.35  # the width of the bars
+datapoints = None
+with open("speedup_data.json", "r") as f:
+    datapoints = json.load(f)
+assert datapoints is not None
 
-    fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/2, men_means, width, label='Men', color = light_blue)
-    rects2 = ax.bar(x + width/2, women_means, width, label='Women', color = dark_blue)
+for i, data in enumerate(datapoints):
+    mark = "Y" if data['success'] else 'n'
+    log(f"[{i+1:3}/{len(datapoints)}]|{data['file']:80}|{mark}")
+    if data["success"] == False:
+        continue
+    labels.append(os.path.basename(data['file']))
+    baseline = data["baseline"]["nthunks"] + data["baseline"]["nconstructs"]
+    optimised = data["optimised"]["nthunks"] + data["optimised"]["nconstructs"]
+    baselines.append(baseline)
+    optims.append(optimised)
 
-    # Y-Axis Label
-    #
-    # Use a horizontal label for improved readability.
-    ax.set_ylabel('Speedup', rotation='horizontal', position = (1, 1.05),
-        horizontalalignment='left', verticalalignment='bottom')
+print(labels)
+print(baselines)
+print(optims)
 
-    # Add some text for labels, title and custom x-axis tick labels, etc.
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
+x = np.arange(len(labels))  # the label locations
+width = 0.35  # the width of the bars
 
-    ax.legend(ncol=100, frameon=False, loc='lower right', bbox_to_anchor=(0, 1, 1, 0))
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, baselines, width, label='Baseline', color = light_blue)
+rects2 = ax.bar(x + width/2, optims, width, label='Optimised', color = dark_blue)
 
-    # Hide the right and top spines
-    #
-    # This reduces the number of lines in the plot. Lines typically catch
-    # a readers attention and distract the reader from the actual content.
-    # By removing unnecessary spines, we help the reader to focus on
-    # the figures in the graph.
-    ax.spines['right'].set_visible(False)
-    ax.spines['top'].set_visible(False)
+# Y-Axis Label
+#
+# Use a horizontal label for improved readability.
+ax.set_ylabel('# instructions overhead', rotation='horizontal', position = (1, 1.05),
+    horizontalalignment='left', verticalalignment='bottom')
 
-    def autolabel(rects):
-        """Attach a text label above each bar in *rects*, displaying its height."""
-        for rect in rects:
-            height = rect.get_height()
-            ax.annotate('{}'.format(height),
-                        xy=(rect.get_x() + rect.get_width() / 2, height),
-                        xytext=(0, 1),  # 1 points vertical offset
-                        textcoords="offset points",
-                        fontsize="smaller",
-                        ha='center', va='bottom')
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_xticks(x)
+ax.set_xticklabels(labels, rotation='horizontal')
+ax.legend(ncol=100, frameon=False, loc='lower right', bbox_to_anchor=(0, 1, 1, 0))
 
-    autolabel(rects1)
-    autolabel(rects2)
+# Hide the right and top spines
+#
+# This reduces the number of lines in the plot. Lines typically catch
+# a readers attention and distract the reader from the actual content.
+# By removing unnecessary spines, we help the reader to focus on
+# the figures in the graph.
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
 
-    fig.tight_layout()
+def autolabel(rects):
+    """Attach a text label above each bar in *rects*, displaying its height."""
+    for rect in rects:
+        height = rect.get_height()
+        ax.annotate('{}'.format(height),
+                    xy=(rect.get_x() + rect.get_width() / 2, height),
+                    xytext=(0, 1),  # 1 points vertical offset
+                    textcoords="offset points",
+                    fontsize="smaller",
+                    ha='center', va='bottom')
 
-    filename = os.path.basename(__file__).replace(".py", ".pdf")
-    fig.savefig(filename)
+autolabel(rects1)
+autolabel(rects2)
+
+fig.tight_layout()
+
+filename = os.path.basename(__file__).replace(".py", ".pdf")
+fig.savefig(filename)
