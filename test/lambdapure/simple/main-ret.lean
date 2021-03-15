27e8f0e99cb58452f87bf634df7ebd8460a9fbd7
Siddharth Bhat <siddu.druid@gmail.com>
2021-03-15 22:43:39 +0530

    Cleanup lean tests
    PASS: HASK_OPT :: lambdapure/simple/main-ret.lean (5 of 9)
    PASS: HASK_OPT :: lambdapure/simple/ctor.lean (6 of 9)
    PASS: HASK_OPT :: lambdapure/simple/const.lean (7 of 9)
    PASS: HASK_OPT :: lambdapure/simple/case.lean (8 of 9)
    PASS: HASK_OPT :: lambdapure/simple/jmp.lean (9 of 9)
    ********************
    Unresolved Tests (3):
      HASK_OPT :: lambdapure/simple/main-print-ret.lean
      HASK_OPT :: lambdapure/simple/main-print.lean
      HASK_OPT :: lambdapure/simple/sub.lean
    
    ********************
    Failed Tests (1):
      HASK_OPT :: lambdapure/simple/proj.lean
    
    Testing Time: 1.03s
      Passed    : 5
      Unresolved: 3
      Failed    : 1

--- test/lambdapure/simple/main-ret.lean
+++ test/lambdapure/simple/main-ret.lean
@@ -1,10 +1,11 @@
---  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | FileCheck %s
---  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  | FileCheck %s
---  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates | FileCheck %s
---  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates --lz-lambdapure-reference-rewriter | FileCheck %s
+--  RUN: lean %s 2>&1 | lambdapure-translate --import-lambdapure | FileCheck %s
+--  RUN: lean %s 2>&1 | lambdapure-translate --import-lambdapure | hask-opt | FileCheck %s
+--  RUN: lean %s 2>&1 | lambdapure-translate --import-lambdapure | hask-opt  --lz-interpret=mode=lambdapure | FileCheck --check-prefix=CHECK-INTERPRET %s
 
 -- CHECK: func @main
+-- 42 is for argv.
+-- CHECK-INTERPRET: constructor(0 7 42)
 
 set_option trace.compiler.ir.init true
 unsafe def main : List String → IO UInt32
-| _ => pure 40
+| _ => pure 7
