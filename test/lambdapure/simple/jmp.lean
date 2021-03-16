d5a7191c6eda12bd260fb65a3aa229c7ccb10508
Siddharth Bhat <siddu.druid@gmail.com>
2021-03-16 19:30:42 +0530

    add simple program to exhibit jmp. Trying to simplify further

--- test/lambdapure/simple/jmp.lean
+++ test/lambdapure/simple/jmp.lean
@@ -1,28 +1,36 @@
+-- Testcase to check a join point / jump instruction
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates | FileCheck %s
 --  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-lambdapure-destructive-updates --lz-lambdapure-reference-rewriter | FileCheck %s
+--  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-interpret="mode=lambdapure stdio=1" | FileCheck %s --check-prefix=CHECK-INTERPRET-1
+--  RUN: lean %s 2>&1 1>/dev/null | lambdapure-translate --import-lambdapure | hask-opt  --lz-interpret="mode=lambdapure stdio=2" | FileCheck %s --check-prefix=CHECK-INTERPRET-2
 
--- : func @constFolding
--- CHECK: module {
+-- CHECK: func @main
 
 set_option trace.compiler.ir.init true
 
 
 inductive Expr
-| Val : Nat → Expr
-| Add : Expr → Expr → Expr
-| Mul : Expr → Expr → Expr
+| Add : Expr -> Expr -> Expr
+| Foo : Nat -> Expr
+| Val : Nat -> Expr
 
 namespace Expr
 open Nat
 
-def constFolding : Expr → Expr
-| Add e1 e2   =>
-  (match e1, e2 with
-   | Val a, Add e (Val b) => Add (Val (a+b)) e
-   | Val a, Add (Val b) e => Add (Val (a+b)) e
-   | _,     _             => Add e1 e2)
-| e         => e
+def eval2 : Expr -> Expr
+ | Add e (Val b) => Add (Val b) e
+ | Add (Val b) e => Add (Val b) e
+ |  x             => x
 
-end Expr
+
+def toNat : Expr -> Nat
+ | Add l r => (toNat l) + (toNat r)
+ | Val n => n
+ | Foo _  => 42
+ 	
+
+unsafe def main (xs: List String) : IO Unit := 
+  IO.println (toString 1) -- (toNat (eval (Val 1) (Val 2))))
+
