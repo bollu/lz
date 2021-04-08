d97078c60d592bec65feda7269d35f0154133d58
Siddharth Bhat <siddu.druid@gmail.com>
2021-04-09 01:34:02 +0530

    add new tests
    commit: 39d837d4cc

--- test/lambdapure/compile/ctor.lean
+++ test/lambdapure/compile/ctor.lean
@@ -1,19 +1,26 @@
---  RUN: lean %s 2>&1 | hask-opt --lz-canonicalize | FileCheck %s
---  RUN: ./run-lean.sh %s | FileCheck %s --check-prefix=CHECK-INTERPRET
+--  RUN: lean %s 2>&1 1>/dev/null | hask-opt --lz-canonicalize | FileCheck %s
+--  RUN: lean %s 2>&1 1>/dev/null | hask-opt --lz-canonicalize  --lz-lambdapure-destructive-updates | FileCheck %s
+--  RUN: lean %s 2>&1 1>/dev/null | hask-opt --lz-canonicalize  --lz-lambdapure-destructive-updates --lz-lambdapure-reference-rewriter | FileCheck %s
 
 -- CHECK-INTERPRET: {{{{{{nil , nil}} , {{nil , nil}}}} , {{{{nil , nil}} , {{nil , nil}}}}}}
 -- CHECK: func @mkTree
 
 set_option trace.compiler.ir.init true
-inductive Ctor
-| MkCtor
-open Ctor
+inductive Tree
+| Nil
+| Node (l r : Tree) : Tree
+open Tree
 
-instance : Inhabited Ctor := ⟨MkCtor⟩
+instance : Inhabited Tree := ⟨Nil⟩
 
-def ctorToStr: Ctor -> String
-| MkCtor => "ctor"
+def mkTree : Nat -> Tree
+| 0  => Nil
+| (x+1) => Node (mkTree (x)) (mkTree (x))
+
+def treeToStr: Tree -> String
+| Nil => "nil"
+| Node l r => "{{" ++ (treeToStr l) ++ " , " ++ (treeToStr r) ++ "}}"
 
 
 def main (xs: List String) : IO Unit := 
-  IO.println (ctorToStr (MkCtor))
+  IO.println (treeToStr (mkTree 3))
