45f35891c73599f8fe81f429a2c0ecfa4c5591b4
Siddharth Bhat <siddu.druid@gmail.com>
2021-04-09 04:48:24 +0530

    add tests that fail
    FAIL: HASK_OPT :: lambdapure/compile/pap.lean (1 of 7)
    PASS: HASK_OPT :: lambdapure/compile/case.lean (2 of 7)
    PASS: HASK_OPT :: lambdapure/compile/ctor.lean (3 of 7)
    PASS: HASK_OPT :: lambdapure/compile/main-print.lean (4 of 7)
    PASS: HASK_OPT :: lambdapure/compile/ctor-simple.lean (5 of 7)
    FAIL: HASK_OPT :: lambdapure/compile/loop.lean (6 of 7)
    FAIL: HASK_OPT :: lambdapure/compile/jmp.lean (7 of 7)
    ********************
    Failed Tests (3):
      HASK_OPT :: lambdapure/compile/jmp.lean
      HASK_OPT :: lambdapure/compile/loop.lean
      HASK_OPT :: lambdapure/compile/pap.lean
    
    Testing Time: 4.34s
      Passed: 4
      Failed: 3

---
+++ test/lambdapure/compile/ctor-simple.lean
@@ -0,0 +1,16 @@
+--  RUN: ./run-lean.sh %s | FileCheck %s --check-prefix=CHECK-INTERPRET
+-- CHECK-INTERPRET: ctor
+
+set_option trace.compiler.ir.init true
+inductive Ctor
+| MkCtor
+open Ctor
+
+instance : Inhabited Ctor := ⟨MkCtor⟩
+
+def ctorToStr: Ctor -> String
+| MkCtor => "ctor"
+
+
+def main (xs: List String) : IO Unit := 
+  IO.println (ctorToStr (MkCtor))
