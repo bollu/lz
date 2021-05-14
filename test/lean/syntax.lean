0252c945fb76e50370d2a63e8f90c9fd060a05a1
Siddharth Bhat <siddu.druid@gmail.com>
2021-05-14 11:30:25 +0530

    add syntax for region.

--- test/lean/syntax.lean
+++ test/lean/syntax.lean
@@ -1,23 +1,53 @@
-
+import Lean.Parser
+-- vim Lean/Parser/Syntax.lean
+-- ~/work/lean4hack/stage1/bin/lean syntax.lean
+-- src/Leanpkg/Toml.lean
+-- Fileworker.lean
 structure inst where
   name : String
+  arg: String
 
+structure MLIRModule where 
+    name: String
+    regions: String
 
-declare_syntax_cat mycat
+declare_syntax_cat newstxcat
+declare_syntax_cat newstxcatregion
 
-syntax ident  : mycat
-syntax "(" mycat ")" : mycat
+syntax ident  : newstxcat
+syntax "(" newstxcat ")" : newstxcat
 
-syntax "to_term"  mycat : term
+syntax "to_term" newstxcat : term
 
 macro_rules
   | `(to_term $a:ident)  => `($a)
   | `(to_term ($a))      => `(to_term $a)
 
-syntax "`[foo|" mycat "]" : term
+syntax "`[foo|" newstxcat "]" : term
 
 macro_rules
   | `(`[foo| $m]) => `(to_term($m))
 
+-- src/Init/Notation.lean
+-- macro_rules | `(tactic| trivial) => `(tactic| assumption)
+macro_rules 
+  | `(RE ER) => `("region")
+
+macro_rules
+  | `(module $rgn) => `(MLIRModule.mk $rgn)
+
+macro_rules
+  | `(XXX $m) => `($m)
+
+macro_rules
+  -- | `(@inst $name:ident ($as:strLit)) => `(inst.mk  $name.getId.toString $as)
+  | `(@inst $name ($as:strLit)) => `(inst.mk  $name $as)
+
 #check `[foo| ((Nat.add))]
+#check XXX Nat.add
+#check @inst "Nat.add" ("foo")
+#check (inst.mk "foo" "bar")
+
+#check (RE   ER)
+#check (MLIRModule.mk "mod" (RE ER))
 
