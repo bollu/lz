3de497136f326d817f0e64a4a3934a1d6ee8f0eb
Siddharth Bhat <siddu.druid@gmail.com>
2021-05-25 23:47:14 +0530

    allow exe-ref.c production to fail because lean needs the path of -c to be within parent

--- test/lambdapure/compile/run-lean.sh
+++ test/lambdapure/compile/run-lean.sh
@@ -5,9 +5,12 @@ set -o xtrace
 
 rm $1-exe.out || true
 
-lean $1 -c exe.c 2>&1 | \
-        hask-opt --lz-canonicalize | tee exe.mlir | \
-        hask-opt  --lean-lower --convert-scf-to-std --ptr-lower | \
+# lean -c fails with relative paths, go figure why.
+lean $1 -c exe-ref.c || true
+
+lean $1 2>&1 | \
+        hask-opt | tee exe.mlir | \
+        hask-opt  --lean-lower  --ptr-lower | \
         mlir-translate --mlir-to-llvmir | tee exe.ll  | llc -filetype=obj -o exe.o
 
 clang -I /home/bollu/work/lean4/build/stage0/include  -S  -emit-llvm exe.c -o exe-lean-ref.ll
