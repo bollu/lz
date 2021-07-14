68ea058ba11547f4d45d9acae032848823414070
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-14 23:38:04 +0530

    update shell scripts as I debug what's going on

--- test/lambdapure/compile/compile-lean.sh
+++ test/lambdapure/compile/compile-lean.sh
@@ -14,10 +14,12 @@ hask-opt exe.mlir | \
   hask-opt  --convert-scf-to-std | hask-opt --lean-lower  | hask-opt --ptr-lower | \
   mlir-translate --mlir-to-llvmir -o exe.ll
 # | opt -S -O3 | llc -filetype=obj -o exe.o
-llvm-link exe.ll /home/bollu/work/lz/lean-linking-incantations/lib-includes/library.ll -S | opt -O3 -S -o exe-linked.ll
+llvm-link exe.ll /home/bollu/work/lz/lean-linking-incantations/lib-includes/library.ll -S | opt --always-inline -O3 -S -o exe-linked.ll
 llc -filetype=obj exe-linked.ll -o exe.o
 
-c++ -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
+# leancpp: apply2, etc
+
+c++ -O3 -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
     exe.o \
     /home/bollu/work/lz/lean-linking-incantations/lean-shell.o \
     -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
--- test/lambdapure/compile/validate-lean.sh
+++ test/lambdapure/compile/validate-lean.sh
@@ -11,6 +11,8 @@ set -o xtrace
 # For whatever reason this does not work with relative paths.
 lean $1 -c exe-ref.c
 leanc exe-ref.c -o exe-ref.out
+c++ -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
+   -O3 -S  exe-ref.c -o exe-ref.ll
 rm out-ref.txt || true
 ./exe-ref.out > ref.txt 
 
