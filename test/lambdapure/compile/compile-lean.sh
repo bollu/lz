e8dc556ccacbe87e7944cc1a39f493d1a66771a7
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-22 17:13:05 +0530

    update compile scripts to use the custom bitcast pass

--- test/lambdapure/compile/compile-lean.sh
+++ test/lambdapure/compile/compile-lean.sh
@@ -14,14 +14,28 @@ hask-opt exe.mlir | \
   hask-opt  --convert-scf-to-std | hask-opt --lean-lower  | hask-opt --ptr-lower | \
   mlir-translate --mlir-to-llvmir -o exe.ll
 # | opt -S -O3 | llc -filetype=obj -o exe.o
-llvm-link exe.ll /home/bollu/work/lz/lean-linking-incantations/lib-includes/library.ll -S | opt --always-inline -O3 -S -o exe-linked.ll
-llc -filetype=obj exe-linked.ll -o exe.o
+llvm-link exe.ll \
+  /home/bollu/work/lz/lean-linking-incantations/lib-includes/library.ll \
+  /home/bollu/work/lz/lean-linking-incantations/lib-runtime/runtime.ll \
+  /home/bollu/work/lz/lean-linking-incantations/lean-shell.ll \
+  -S -o exe-linked.ll
+opt exe-linked.ll -passes=bitcast-call-converter -S -o exe-linked-nobitcast.ll
+opt -always-inline -O3 exe-linked-nobitcast.ll -S -o exe-linked-o3.ll
+# opt exe-linked.ll  --always-inline -O3 -S -o exe-linked-o3.ll
+llc -filetype=obj exe-linked-o3.ll -o exe.o
 
 # leancpp: apply2, etc
 
+# suspect #1: the lean-shell.o
 c++ -O3 -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
     exe.o \
-    /home/bollu/work/lz/lean-linking-incantations/lean-shell.o \
     -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
     -L/home/bollu/work/lean4/build/stage1/lib/lean -lgmp -ldl -pthread \
     -Wno-unused-command-line-argument -o exe.out
+
+# c++ -O3 -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
+#     exe.o \
+#     /home/bollu/work/lz/lean-linking-incantations/lean-shell.o \
+#     -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
+#     -L/home/bollu/work/lean4/build/stage1/lib/lean -lgmp -ldl -pthread \
+#     -Wno-unused-command-line-argument -o exe.out
--- test/lambdapure/compile/validate-lean.sh
+++ test/lambdapure/compile/validate-lean.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+
 # https://stackoverflow.com/a/23161454/5305365
 SCRIPTDIR="${BASH_SOURCE[0]}";
 SCRIPTDIR=$(dirname $SCRIPTDIR)
@@ -11,11 +12,14 @@ set -o xtrace
 # For whatever reason this does not work with relative paths.
 lean $1 -c exe-ref.c
 leanc -O3 exe-ref.c -o exe-ref.out
-c++ -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
-   -O3 -S  exe-ref.c -o exe-ref.ll
+clang++ -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
+   -O3 -S  -emit-llvm  exe-ref.c -o exe-ref.ll
 rm out-ref.txt || true
 ./exe-ref.out > ref.txt 
 
 rm out-ours.txt || true
 $SCRIPTDIR/run-lean.sh $1 > ours.txt 
 diff ref.txt ours.txt
+
+time ./exe-ref.out
+time ./exe.out
