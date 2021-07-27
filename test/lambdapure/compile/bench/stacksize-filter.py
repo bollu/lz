52cf0c933572c76fc74abd55aa0bde756ee8c24a
Siddharth Bhat <siddu.druid@gmail.com>
2021-07-27 21:23:47 +0530

    add stacksize plot, finally get it to render riht

--- test/lambdapure/compile/bench/stacksize-filter.py
+++ test/lambdapure/compile/bench/stacksize-filter.py
@@ -31,7 +31,6 @@ ARGS = PARSER.parse_args()
 G_BASELINE = "../baseline-lean.sh"
 G_OURS = "../run-lean.sh"
 FPATH="filter-tail-test.lean"
-PROBLEM_SIZES=[2**i for i in range(2, 20)]
 
 # Color palette
 light_gray = "#cacaca"
@@ -47,69 +46,81 @@ dark_red = "#e31a1c"
 def log(*ARGS, **kwargs):
     print(*ARGS, file=sys.stderr, **kwargs)
 
-def os_system_synch(path):
-    return subprocess.check_output(path, shell=True)
+class ShResult:
+  def __init__(self, command: str, stdout: str, stderr:str, retcode:int):
+    self.command = command
+    self.stdout = stdout
+    self.stderr = stderr 
+    self.retcode = retcode
 
-def sh(path):
-    proc = subprocess.Popen(path, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
-    output = proc.stdout.read()
-    err = proc.stderr.read()
-    return (output.decode(), err.decode())
+  def assert_success(self):
+    assert self.retcode == 0
+  
+  @property
+  def success(self):
+    return self.retcode == 0
+
+def sh(command: str) -> ShResult:
+    proc = subprocess.Popen(command, shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
+    stdout = proc.stdout.read()
+    stderr = proc.stderr.read()
+    proc.wait()
+    return ShResult(command, stdout.decode(),stderr.decode(), proc.returncode)
 
 
 def run_data():
     ds = []
-    os_system_synch(f"rm exe-ref.out")
-    os_system_synch(f"lean {FPATH} -c exe-ref.c")
-    os_system_synch(f"leanc exe-ref.c -O3 -o exe-ref.out")
+    sh(f"rm exe-ref.out")
+    sh(f"lean {FPATH} -c exe-ref.c")
+    sh(f"leanc exe-ref.c -O3 -o exe-ref.out")
 
-    os_system_synch(f"rm exe-mlir.out")
-    os_system_synch(f"lean {FPATH} -m exe.mlir")
-    os_system_synch("hask-opt exe.mlir --convert-scf-to-std --lean-lower --ptr-lower | \
+    sh(f"rm exe-mlir.out")
+    sh(f"lean {FPATH} -m exe.mlir")
+    sh("hask-opt exe.mlir --convert-scf-to-std --lean-lower --ptr-lower | \
             mlir-translate --mlir-to-llvmir -o exe.ll")
-    os_system_synch("llvm-link " + 
+    sh("llvm-link " + 
               "exe.ll " + 
               "/home/bollu/work/lz/lean-linking-incantations/lib-includes/library.ll " + 
               "/home/bollu/work/lz/lean-linking-incantations/lib-runtime/runtime.ll " +
               "| opt -passes=bitcast-call-converter  | opt --always-inline -O3 -S -o exe-linked.ll")
     print("@@@ HACK: converting muttail to tail because of llc miscompile@@@")
-    os_system_synch("sed -i s/musttail/tail/g exe-linked.ll")
-    os_system_synch("sed -i s/musttail/tail/g exe-linked.ll")
-    os_system_synch("llc -O3 -march=x86-64 -filetype=obj exe-linked.ll -o exe.o")
-    os_system_synch(f"c++ -O3 -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
+    sh("sed -i s/musttail/tail/g exe-linked.ll")
+    sh("sed -i s/musttail/tail/g exe-linked.ll")
+    sh("llc -O3 -march=x86-64 -filetype=obj exe-linked.ll -o exe.o")
+    sh(f"c++ -O3 -D LEAN_MULTI_THREAD -I/home/bollu/work/lean4/build/stage1/include \
         exe.o \
         /home/bollu/work/lz/lean-linking-incantations/lean-shell.o \
         -no-pie -Wl,--start-group -lleancpp -lInit -lStd -lLean -Wl,--end-group \
         -L/home/bollu/work/lean4/build/stage1/lib/lean -lgmp -ldl -pthread \
         -Wno-unused-command-line-argument -o exe-mlir.out")
-        # os_system_synch("opt -O3 exe-linked.ll -o exe-linked-o3.ll")
-        # os_system_synch("mv exe-linked-o3.ll exe-linked.ll")
+        # sh("opt -O3 exe-linked.ll -o exe-linked-o3.ll")
+        # sh("mv exe-linked-o3.ll exe-linked.ll")
 
-    for psz in PROBLEM_SIZES:
-        datum = {"problem_size": str(psz) }
+    for two_pow in range(2, 20):
+
+        datum = {"problem_size": two_pow } # logarithmic
+        psz = 2**two_pow
         # x = run_with_output("perf stat ./exe-ref.out 1>/dev/null")
         # proc = subprocess.Popen("perf stat ./exe-ref.out", shell=True, stderr=subprocess.PIPE, stdout=subprocess.PIPE)
         datum["theirs-out"] = []
         datum["theirs-perf"] = []
         for _ in range(ARGS.nruns):
-          os_system_synch("rm massif.out")
-          out = sh(f"valgrind --massif-out-file=massif.out --stacks=yes --tool=massif ./exe-ref.out {psz}");
-          massif = msparser.parse_file('massif.out')
-          datum["theirs-out"].append(out)
-          datum["theirs-perf"].append([snap['mem_stack'] for snap in massif['snapshots']])
+          sh("rm massif.out")
+          exe_ref = sh(f"valgrind --massif-out-file=massif.out --stacks=yes --tool=massif ./exe-ref.out {psz}");
+          if exe_ref.success:
+            massif = msparser.parse_file('massif.out')
+            datum["theirs-out"].append(exe_ref.stdout)
+            datum["theirs-perf"].append([snap['mem_stack'] for snap in massif['snapshots']])
 
         datum["ours-out"] = []
         datum["ours-perf"] = []
         for _ in range(ARGS.nruns):
-           os_system_synch("rm massif.out")
-           out = sh(f"valgrind --massif-out-file=massif.out --stacks=yes --tool=massif ./exe-mlir.out {psz}");
-           massif = msparser.parse_file('massif.out')
-           datum["ours-perf"].append([snap['mem_stack'] for snap in massif['snapshots']])
-           datum["ours-out"].append(out)
-           # out, perf = sh("valgrind stat ./exe-mlir.out")
-           # datum["ours-out"].append(out)
-           # datum["ours-perf"].append(perf)
-
+           sh("rm massif.out")
+           exe = sh(f"valgrind --massif-out-file=massif.out --stacks=yes --tool=massif ./exe-mlir.out {psz}");
+           if exe.success:
+             massif = msparser.parse_file('massif.out')
+             datum["ours-perf"].append([snap['mem_stack'] for snap in massif['snapshots']])
+             datum["ours-out"].append(exe.stdout)
         ds.append(datum)
     with open(ARGS.out, "w") as f:
         json.dump(ds, f, indent=2)
@@ -159,22 +170,13 @@ def plot():
       # if data["success"] == False:
       #     continue
       labels.append(data["problem_size"])
-      N = len(data["theirs-perf"])
-      assert N == len(data["ours-perf"])
 
       # take maximum of stack size used.
       print(data["theirs-perf"])
-      theirs.append(np.log(np.median([np.max(run) for run in data["theirs-perf"]])))
-      ours.append(np.log(np.median([np.max(run) for run in data["ours-perf"]])))
-      # speedup = float("%4.2f" % (theirs/ours))
-      # speedups.append(speedup)
-      # optims.append(speedup)
-
-  # avg_speedup = mstats.gmean(speedups)
-  # print("average speedup: %4.2f" % (avg_speedup, ))
-  # baselines.append(1)
-  # optims.append(float("%4.2f" % (avg_speedup)))
-  # labels.append("geomean")
+      if data["theirs-perf"]:
+        theirs.append(np.log2(np.median([np.max(run) for run in data["theirs-perf"]])))
+      if data["ours-perf"]:
+        ours.append(np.log2(np.median([np.max(run) for run in data["ours-perf"]])))
 
   print(labels)
   print(theirs)
@@ -184,8 +186,8 @@ def plot():
   width = 0.35  # the width of the bars
 
   fig, ax = plt.subplots()
-  rects1 = ax.bar(x - width/2, theirs, width, label='theirs', color = light_blue)
-  rects2 = ax.bar(x + width/2, ours, width, label='ours', color = dark_blue)
+  rects1 = ax.bar(x[:len(theirs)] - width/2, theirs, width, label='theirs', color = light_blue)
+  rects2 = ax.bar(x[:len(ours)] + width/2, ours, width, label='ours', color = dark_blue)
   # rects2[-1].set_color(light_blue) # color geomean separately.
 
 
@@ -195,14 +197,14 @@ def plot():
   # Y-Axis Label
   #
   # Use a horizontal label for improved readability.
-  ax.set_ylabel('Stack size on filter.lean for different problem sizes', rotation='horizontal', position = (1, 1.1),
+  ax.set_ylabel('log2(peak # stack frames)', rotation='horizontal', position = (1, 1.1),
       horizontalalignment='left', verticalalignment='bottom', fontsize=8)
 
   # Add some text for labels, title and custom x-axis tick labels, etc.
   ax.set_xticks(x)
   ax.set_xticklabels(labels, rotation=15, fontsize=7)
   ax.legend(ncol=100, frameon=False, loc='lower right', bbox_to_anchor=(0, 1, 1, 0))
-
+  ax.set_xlabel('log2(problem size)', fontsize=8)
   # Hide the right and top spines
   # This reduces the number of lines in the plot. Lines typically catch
   # a readers attention and distract the reader from the actual content.
@@ -210,8 +212,8 @@ def plot():
   # the figures in the graph.
   ax.spines['right'].set_visible(False)
   ax.spines['top'].set_visible(False)
-  autolabel(ax, rects1)
-  autolabel(ax, rects2)
+  # autolabel(ax, rects1)
+  # autolabel(ax, rects2)
 
   fig.tight_layout()
   filename = os.path.basename(__file__).replace(".py", ".jpg")
