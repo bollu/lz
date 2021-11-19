52ccc3f8d88354d95f190f89bb778630d1b168c2
Siddharth Bhat <siddu.druid@gmail.com>
2021-11-19 12:20:59 +0530

    add runtime creator file

--- src/runtime/mk-runtime.sh
+++ src/runtime/mk-runtime.sh
@@ -1,17 +1,24 @@
+#!/usr/bin/env bash
 # ./src/config.h.in
 # ./build/stage0/include/lean/config.h
 set -o xtrace
 set -e 
 
-LEANCC="clang -I $HOME/work/lean4/build/stage0/include -I $HOME/work/lean4/src/"
-$LEANCC -O3 alloc.cpp -S -emit-llvm -o alloc.ll 
-$LEANCC -O3 object.cpp -S -emit-llvm -o object.ll 
-$LEANCC -O3 apply.cpp -S -emit-llvm -o apply.ll 
-llvm-link alloc.ll object.ll apply.ll -S -o runtime.ll
+FILES=$(ls -1u *.cpp)
+LLFILES=""
+LEANCC="clang -O3 -DLEAN_MULTI_THREAD -I $HOME/work/lean4/build/release/stage0/include -I $HOME/work/lean4/src/"
+
+for file in $FILES; do 
+    i=$(basename $file .cpp)
+    LLFILES="$LLFILES $i.ll"
+    $LEANCC -O3 $file -S -emit-llvm  -o  $i.ll
+done
+
+echo $LLFILES
+
 # alwaysinline causes runtime to take infinite amounts of time.
 sed -i s/noinline/alwaysinline/g runtime.ll
-# sed -i s/noinline//g runtime.ll
 sed -i s/optnone//g runtime.ll
 opt -S -O3 runtime.ll -o runtime-optimized.ll
 mv runtime-optimized.ll runtime.ll
-cp ~/work/lean4/src/runtime/runtime.ll ~/work/lz/lean-linking-incantations/lib-runtime
+cp /code/lean4/src/runtime/runtime.ll /code/lz/lean-linking-incantations/lib-runtime/
